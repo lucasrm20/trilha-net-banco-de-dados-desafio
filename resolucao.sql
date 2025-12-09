b0e92db7628ef009f773e84265f3cc5556554078
Lucas Rodrigues Moura <lucas.rodrigues20@outlook.com>
2025-12-09 20:22:40 -0300

    resolucao

---
+++ resolucao.sql
@@ -0,0 +1,94 @@
+-- 1
+SELECT
+  Nome,
+  Ano
+FROM Filmes
+
+-- 2
+SELECT
+  Nome,
+  Ano,
+  Duracao
+FROM Filmes
+ORDER BY Ano
+
+-- 3
+SELECT
+  Nome,
+  Ano,
+  Duracao
+FROM Filmes
+WHERE Nome LIKE '%De Volta para o Futuro%';
+
+-- 4
+SELECT
+  Nome,
+  Ano,
+  Duracao
+FROM Filmes
+WHERE Ano = 1997;
+
+-- 5
+SELECT
+  Nome,
+  Ano,
+  Duracao
+FROM Filmes
+WHERE Ano > 2000;
+
+-- 6
+SELECT
+  Nome,
+  Ano,
+  Duracao
+FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao;
+
+-- 7
+SELECT
+  Ano,
+  COUNT(*) Quantidade
+FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC;
+
+-- 8
+SELECT
+  *
+FROM Atores
+WHERE Genero='M';
+
+-- 9
+SELECT
+  *
+FROM Atores
+WHERE Genero='F'
+ORDER BY PrimeiroNome;
+
+-- 10
+SELECT
+  Filmes.Nome,
+  Generos.Genero
+FROM Filmes
+INNER JOIN FilmesGenero ON FilmesGenero.idFilme = Filmes.id
+INNER JOIN Generos ON Generos.id = FilmesGenero.idGenero;
+
+-- 11
+SELECT
+  Filmes.Nome,
+  Generos.Genero
+FROM Filmes
+INNER JOIN FilmesGenero ON FilmesGenero.idFilme = Filmes.id
+INNER JOIN Generos ON Generos.id = FilmesGenero.idGenero
+WHERE Generos.Genero='Mistério';
+
+-- 12
+SELECT
+  Filmes.Nome,
+  Atores.PrimeiroNome,
+  Atores.UltimoNome,
+  ElencoFilme.Papel
+FROM Filmes
+INNER JOIN ElencoFilme ON ElencoFilme.idFilme = Filmes.id
+INNER JOIN Atores ON Atores.id = ElencoFilme.idAtor;
